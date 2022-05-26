83f691de1753b14db3055cc575f0d40e17790533
oswi <oswaldo.adrian@live.com.mx>
2022-05-26 18:22:51 -0500

    product service

---
+++ src/main/java/com/spring/market/springmarket/service/ProductService.java
@@ -0,0 +1,44 @@
+package com.spring.market.springmarket.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.spring.market.springmarket.model.dto.ProductDto;
+import com.spring.market.springmarket.repository.ProductRepository;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+  
+  private final ProductRepository productRepository;
+
+  public ProductService(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
+
+  public List<ProductDto> getAll() {
+    return productRepository.getAll();
+  }
+
+  public Optional<ProductDto> getProduct(int productId) {
+    return productRepository.getProduct(productId);
+  }
+
+  Optional<List<ProductDto>> getByCategoryId(int categoryId) {
+    return productRepository.getByCategoryId(categoryId);
+  }
+
+  public ProductDto save(ProductDto product) {
+    return productRepository.save(product);
+  }
+
+  public boolean delete(int productId) {
+    return getProduct(productId)
+      .map(product -> {
+        productRepository.delete(productId);
+        return true;
+      }).orElse(false);
+  }
+
+}
